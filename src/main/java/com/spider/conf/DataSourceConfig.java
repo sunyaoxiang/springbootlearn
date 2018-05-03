ecbac040ea1985bfdcffcb6b03e8331559e37a02
yaoxiang.sun <yaoxiang.sun@datayes.com>
2018-05-03 20:02:37 +0800

    1.for learn

---
+++ src/main/java/com/spider/conf/DataSourceConfig.java
@@ -0,0 +1,65 @@
+package com.spider.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.Map;
+
+/**
+ * Created by yaoxiang.sun on 2018/5/3.
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "entityManagerFactorySpider",
+        transactionManagerRef = "entityManagerFactorySpider",
+        basePackages = {"com.spider.service"}
+)
+public class DataSourceConfig {
+    @Autowired
+    @Qualifier("spiderdbSource")
+    private DataSource spiderdbSource;
+
+    @Primary
+    @Bean(name = "entityManagerPrimary")
+    public EntityManager entityManager(EntityManagerFactoryBuilder builder) {
+        return entityManagerFactoryPrimary(builder).getObject().createEntityManager();
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactoryPrimary")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryPrimary (EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(spiderdbSource)
+                .properties(getVendorProperties(spiderdbSource))
+                .packages("com.spider.service")
+                .persistenceUnit("persistenceUnit")
+                .build();
+    }
+
+    @Autowired
+    private JpaProperties jpaProperties;
+
+    private Map<String, String> getVendorProperties(DataSource dataSource) {
+        return jpaProperties.getHibernateProperties(dataSource);
+    }
+
+    @Primary
+    @Bean(name = "transactionManagerPrimary")
+    public PlatformTransactionManager transactionManagerPrimary(EntityManagerFactoryBuilder builder) {
+        return new JpaTransactionManager(entityManagerFactoryPrimary(builder).getObject());
+    }
+
+}
--- src/main/java/com/spider/controller/BondSpiderAnnoInfoController.java
+++ src/main/java/com/spider/controller/BondSpiderAnnoInfoController.java
@@ -4,8 +4,11 @@ import com.spider.entity.BondSpiderAnnoInfo;
 import com.spider.service.ServeiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -16,45 +19,21 @@ public class BondSpiderAnnoInfoController {
 
     @Autowired
     private ServeiceInterface serveiceInterface;
-    @RequestMapping("/d")
-//    @ResponseBody
-    public String getWorkDay() {
 
+    //    @RequestMapping("/d")
+    @RequestMapping(value = {"/api/v1/test"}, method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String getWorkDay(HttpServletResponse response) {
+//    public BondSpiderAnnoInfo getWorkDay(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
 
         List<BondSpiderAnnoInfo> bondSpiderAnnoInfos = serveiceInterface.findByTickerSymbol("1880048");
 
-        for (BondSpiderAnnoInfo entity :bondSpiderAnnoInfos) {
+        for (BondSpiderAnnoInfo entity : bondSpiderAnnoInfos) {
             if (entity != null) {
-                return entity.getTickerSymobol() + "|" + entity.getSecShortName() + "|" +  entity.getPublishDate();
+                return entity.getTickerSymobol() + "|" + entity.getSecShortName() + "|" + entity.getPublishDate();
             }
         }
-
         return "-1";
     }
-
-//    @Autowired
-//    private BondSpiderAnnoInfoServeice bondSpiderAnnoInfoServeice;
-//
-//    @RequestMapping(path ="/getByPulishDate", value = "2018-04-22")
-//    @ResponseBody
-//    public String getBondSpiderAnnoInfoFindByPulishDate(@RequestParam(value = "2018-04-22") String publishDate) {
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//        Date date = null;
-//        try {
-//            date = dateFormat.parse(publishDate);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//
-//        List<BondSpiderAnnoInfo> bondSpiderAnnoInfoList = bondSpiderAnnoInfoServeice.findByPulishDate(date);
-//
-//
-//        if (bondSpiderAnnoInfoList.isEmpty() != false) {
-//            for (BondSpiderAnnoInfo bondSpiderAnnoInfo : bondSpiderAnnoInfoList) {
-//                return bondSpiderAnnoInfo.getTickerSymobol();
-//            }
-//        }
-//        return "-1";
-//    }
-
 }
