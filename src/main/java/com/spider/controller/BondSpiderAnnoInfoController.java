91b6972bcc7ed8733f3711799b80bc869b95dc21
sunyaoxiang <306289465@163.com>
2018-05-24 22:45:54 +0800

    1.return a view.

--- src/main/java/com/spider/controller/BondSpiderAnnoInfoController.java
+++ src/main/java/com/spider/controller/BondSpiderAnnoInfoController.java
@@ -3,9 +3,9 @@ package com.spider.controller;
 import com.spider.entity.BondSpiderAnnoInfo;
 import com.spider.service.ServeiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,26 +14,24 @@ import java.util.List;
 /**
  * Created by yaoxiang.sun on 2018/5/3.
  */
-@RestController
+@Controller
+@RequestMapping("/annoinfo")
 public class BondSpiderAnnoInfoController {
 
     @Autowired
     private ServeiceInterface serveiceInterface;
 
-    //    @RequestMapping("/d")
-    @RequestMapping(value = {"/api/v1/test"}, method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json;charset=UTF-8")
-    @ResponseBody
-    public String getWorkDay(HttpServletResponse response) {
-//    public BondSpiderAnnoInfo getWorkDay(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+//    @RequestMapping(value = "/list", method = {RequestMethod.GET, RequestMethod.POST})
+//    , produces = "application/json;charset=UTF-8"
+//    @ResponseBody
+    @RequestMapping("/list")
+    public String listAnnoInfo(Model model) {
+//        response.addHeader("Access-Control-Allow-Origin", "*");
 
-        List<BondSpiderAnnoInfo> bondSpiderAnnoInfos = serveiceInterface.findByTickerSymbol("1880048");
+        List<BondSpiderAnnoInfo> bondSpiderAnnoInfos = serveiceInterface.findAll();
 
-        for (BondSpiderAnnoInfo entity : bondSpiderAnnoInfos) {
-            if (entity != null) {
-                return entity.getTickerSymobol() + "|" + entity.getSecShortName() + "|" + entity.getPublishDate();
-            }
-        }
-        return "-1";
+
+        model.addAttribute("anninfos", bondSpiderAnnoInfos);
+        return "/annoinfo/list";
     }
 }
--- src/main/java/com/spider/service/ServeiceInterface.java
+++ src/main/java/com/spider/service/ServeiceInterface.java
@@ -13,32 +13,6 @@ import java.util.List;
 public interface ServeiceInterface extends CrudRepository<BondSpiderAnnoInfo, String> {
 
     List<BondSpiderAnnoInfo> findByTickerSymbol(String TICKER_SYMBOL);
+    List<BondSpiderAnnoInfo> findAll();
 
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//
-//    public List<BondSpiderAnnoInfo> findByPulishDate(Date publishDate) {
-//        String sql = "SELECT " +
-//                "`id`," +
-//                "`tickerSymbol`," +
-//                "`secShortName`," +
-//                " `publishDate`" +
-//                "  FROM " +
-//                "`bond_spider_anno_info` " +
-//                "where " +
-//                "`PUBLISH_DATE` = \'" + publishDate + "\'";
-//
-//        return (List<BondSpiderAnnoInfo>) jdbcTemplate.query(sql, new RowMapper<BondSpiderAnnoInfo>() {
-//
-//            @Override
-//            public BondSpiderAnnoInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
-//                BondSpiderAnnoInfo bondSpiderAnnoInfo = new BondSpiderAnnoInfo();
-//                bondSpiderAnnoInfo.setId(rs.getLong("ID"));
-//                bondSpiderAnnoInfo.setTickerSymobol(rs.getString("TICKER_SYMBOL"));
-//                bondSpiderAnnoInfo.setSecShortName(rs.getString("SEC_SHORT_NAME"));
-//                bondSpiderAnnoInfo.setPublishDate(rs.getDate("PUBLISH_DATE"));
-//                return bondSpiderAnnoInfo;
-//            }
-//        });
-//    }
 }
